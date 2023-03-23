10c9763bbee42161db7e91392b63ce15917e321c
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-03-23 17:27:56 +0100

    add task 1 + 2

--- src/main/scala/u03/Lab.scala
+++ src/main/scala/u03/Lab.scala
@@ -1,20 +1,72 @@
 package u03
 
 import u03.Lists.*
+import u02.Optionals.*
+import u02.Modules.*
 
 import scala.annotation.tailrec
 
 object Lab extends App:
   import List.*
+  import Option.*
 
+  // Task 1.1.a
   @tailrec
   def drop[A](list: List[A], index: Int): List[A] = (list, index) match
     case (l, 0) => l
     case (Nil(), _) => Nil()
     case (Cons(_, t), i) => drop(t, i - 1)
 
+  // Task 1.1.b
   def append[A](left: List[A], right: List[A]): List[A] = left match
     case Nil() => right
     case Cons(h, Nil()) => Cons(h, right)
     case Cons(h, t) => Cons(h, append(t, right))
+
+  // Task 1.1.c
+  def flatMap[A, B](list: List[A])(f: A => List[B]): List[B] = list match
+    case Nil() => Nil()
+    case Cons(h, Nil()) => f(h)
+    case Cons(h, t) => append(f(h), flatMap(t)(f))
+
+  // Task 1.1.d
+  def maps[A, B](list: List[A])(f: A => B): List[B] = flatMap(list)(v => Cons(f(v), Nil()))
+
+  def filters[A](list: List[A])(predicate: A => Boolean): List[A] =
+    flatMap(list)(a => predicate(a) match
+      case true => Cons(a, Nil())
+      case _ => Nil()
+    )
+
+  // Task 1.2
+  def max(list: List[Int]): Option[Int] = list match
+    case Nil() => None()
+    case Cons(h, Nil()) => Some(h)
+    case Cons(h, t) => max(t) match
+      case Some(x) if x > h => Some(x)
+      case _ => Some(h)
+
+  // Task 2.2
+  def foldLeft[A, B](list: List[A])(initial: B)(aggregator: (B, A) => B): B = list match
+    case Cons(h, t) => foldLeft(t)(aggregator(initial, h))(aggregator)
+    case Nil() => initial
+
+  def foldRight[A, B](list: List[A])(initial: B)(aggregator: (A, B) => B): B =
+    def reverse[E](list: List[E]): List[E] = list match
+      case Cons(_, Nil()) => list
+      case Cons(h, t) => append(reverse(t), Cons(h, Nil()))
+      case Nil() => Nil()
+    foldLeft(reverse(list))(initial)((b, a) => aggregator(a, b))
+
+  // Task 2.1
+  import Person.*
+
+//  def coursesOf(list: List[Person]): List[String] =
+//    maps(filters(list)(p => !isStudent(p)))(p => p match { case Teacher(_, c) => c} )
+
+  def coursesOf(list: List[Person]): List[String] =
+    flatMap(list)(_ match
+      case Teacher(_, c) => Cons(c, Nil())
+      case _ => Nil()
+    )
 
--- src/test/scala/u03/LabTest.scala
+++ src/test/scala/u03/LabTest.scala
@@ -3,6 +3,8 @@ package u03
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import u03.Lists.List
+import u02.Optionals.Option
+import u02.Modules.{Person, isStudent}
 
 class LabTest:
   import List.*
@@ -23,3 +25,48 @@ class LabTest:
     assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Nil())))), append(list, Cons(40, Nil())))
     assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), append(list, Nil()))
     assertEquals(Cons(40, Nil()), append(Nil(), Cons(40, Nil())))
+
+  @Test
+  def testFlatMap(): Unit =
+    assertEquals(Nil(), flatMap(Nil[Int]())(v => Cons(v + 1, Nil())))
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(list)(v => Cons(v + 1, Nil())))
+    assertEquals(
+      Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))),
+      flatMap(list)(v => Cons(v + 1, Cons(v + 2, Nil())))
+    )
+
+  @Test
+  def testMap(): Unit =
+    assertEquals(Nil(), maps(Nil[Int]())(v => v * 3))
+    assertEquals(Cons(1, Cons(2, Cons(3, Nil()))), maps(list)(v => v / 10))
+
+  import Person.*
+  @Test
+  def testFilter(): Unit =
+    assertEquals(list, filters(list)(v => true))
+    assertEquals(Nil(), filters(list)(v => v > 30))
+    assertEquals(Cons(10, Cons(20, Nil())), filters(list)(v => v < 30))
+
+  import Option.*
+  @Test
+  def testMax(): Unit =
+    assertEquals(Some(30), max(list))
+    assertEquals(None(), max(Nil()))
+
+  @Test
+  def testFoldLeft(): Unit =
+    assertEquals(10, foldLeft(Nil[Int]())(10)(_ - _))
+    assertEquals(-60, foldLeft(list)(0)(_ - _))
+    assertEquals(1, foldLeft(list)(6_000)(_ / _))
+    assertEquals("a102030", foldLeft(list)("a")((i, v) => i.concat(s"$v")))
+
+  @Test
+  def testFoldRight(): Unit =
+    assertEquals(20, foldRight(list)(0)(_ - _))
+    assertEquals(5, foldRight(list)(3)(_ / _))
+    assertEquals("102030a", foldRight(list)("a")((v, i) => s"$v".concat(i) ))
+
+  @Test
+  def testCoursesOf(): Unit =
+    val p = Cons(Teacher("Pianini", "oop"), Cons(Student("Tassinari", 2000), Cons(Teacher("Ricci", "pcd"), Nil())))
+    assertEquals(Cons("oop", Cons("pcd", Nil())), coursesOf(p))
